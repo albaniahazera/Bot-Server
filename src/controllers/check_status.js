3a90e12fc5d5ca18ae24f138dea6919550d56f28
albaniahazera <alzgamer2005@gmail.com>
2025-12-11 17:39:05 +0700

    V-1.6.0

--- src/controllers/check_status.js
+++ src/controllers/check_status.js
@@ -38,7 +38,23 @@ exports.cpu_info = (async (req, res) => {
             brand: cpu.brand,
             speed: cpu.speed,
             cores: cpu.cores,
-            physicalCores: cpu.physicalCores
+            cache: cpu.cache,
+            family: cpu.family,
+            flags: cpu.flags,
+            governor: cpu.governor,
+            model: cpu.model,
+            performanceCores: cpu.performanceCores,
+            efficiencyCores: cpu.efficiencyCores,
+            processors: cpu.processors,
+            revision: cpu.revision,
+            socket: cpu.socket,
+            speed_max: cpu.speedMax,
+            speed_min: cpu.speedMin,
+            stepping: cpu.stepping,
+            verndor: cpu.vendor,
+            virtualization: cpu.virtualization,
+            voltage: cpu.voltage,
+            physicalCores: cpu.physicalCores   
         });
     }catch (error) {
         console.error('Error retrieving CPU data:', error.message);
@@ -54,8 +70,21 @@ exports.memory_info = (async (req, res) => {
             return res.status(404).json({ error: 'No Memory data found' });
         }
 
+        const first_memory = memory[0];
+
         res.json({
-            memory: memory
+            memory_total: first_memory.size,
+            memory_bank: first_memory.bank,
+            memory_clock_speed: first_memory.clockSpeed,
+            memory_ecc: first_memory.ecc,
+            memory_form_factor: first_memory.formFactor,
+            memory_manufacturer: first_memory.manufacturer,
+            memory_part_number: first_memory.partNum,
+            memory_serial_number: first_memory.serialNum,
+            memory_type: first_memory.type,
+            memory_vol_config: first_memory.voltageConfigured,
+            memory_vol_max: first_memory.voltageMax,
+            memory_vol_min: first_memory.voltageMin
         });
     }catch (error) {
         console.error('Error retrieving Memory data:', error.message);
@@ -69,6 +98,22 @@ exports.disk_info = (async (req, res) => {
         res.json(disk.map(d => ({
             type: d.type,
             name: d.name,
+            bytes_per_sector: d.bytesPerSector,
+            device: d.device,
+            firmware_revision: d.firmwareRevision,
+            interface_type: d.interfaceType,
+            sectors_per_track: d.sectorsPerTrack,
+            serial_num: d.serialNum,
+            size: d.size,
+            smart_data: d.smartData,
+            smart_status: d.smartStatus,
+            temperature: d.temperature,
+            total_cylinders: d.totalCylinders,
+            total_heads: d.totalHeads,
+            total_sectors: d.totalSectors,
+            total_tracks: d.totalTracks,
+            tracks_per_cylinder: d.tracksPerCylinder,
+            vendor: d.vendor
         })));
     }catch (error) {
         console.error('Error retrieving Disk data:', error.message);
@@ -84,10 +129,84 @@ exports.os_info = (async (req, res) => {
             distro: os_info.distro,
             release: os_info.release,
             kernel: os_info.kernel,
-            arch: os_info.arch
+            arch: os_info.arch,
+            build: os_info.build,
+            code_name: os_info.codename,
+            code_page: os_info.codepage,
+            fqdn: os_info.fqdn,
+            host_name: os_info.hostname,
+            hypervizor: os_info.hypervizor,
+            logo_file: os_info.logofile,
+            remote_session: os_info.remoteSession,
+            serial: os_info.serial,
+            service_pack: os_info.servicepack,
+            uefi: os_info.uefi
         });
     }catch (error) {
         console.error('Error retrieving OS data:', error.message);
         res.status(500).json({ error: 'Failed to retrieve OS data' });
+    }
+});
+
+exports.network_info = (async (req, res) => {
+    try {
+        const net_stat = await si.networkStats();
+        let network_data = {};
+
+        net_stat.forEach(iface => {
+            const name = iface.iface;
+            let type = null;
+
+            if(name.startsWith('wl')) {
+                type = 'Wireless (wifi)'
+            }else if (name.startsWith('en')) {
+                type = 'Ethernet (wired)'
+            }else if (name.startsWith('lo')) {
+                return;
+            }
+            
+            if(type) {
+                const rx_mb = (iface.rx_sec / (1024 * 1024)).toFixed(2);
+                const tx_mb = (iface.tx_sec / (1024 * 1024)).toFixed(2);
+            
+                network_data[name] = {
+                    interface: name,
+                    type: type,
+                    bytes_received_MB: rx_mb,
+                    bytes_sent_MB: tx_mb,
+                    packets_received: iface.rx_sec,
+                    packets_sent: iface.tx_sec
+                };
+            };
+        });
+
+        if(Object.keys(network_data).length === 0) {
+            return res.status(404).json({ error: 'No active Ethernet (en) or Wireless (wl) interfaces found.' });
+        }
+
+        res.json(network_data);
+    }catch (error){
+        console.error('Error retrieving network stats:', error.message);
+        res.status(500).json({ error: 'Failed to retrieve network stats' });
+    }
+});
+
+exports.process_info = (async (req, res) => {
+    try {
+        const data = await si.processes();
+        const top_processes = data.list
+        .sort((a, b) => b.cpu - a.cpu)
+        .slice(0, 10);
+
+        res.json(top_processes.map(p => ({
+            pid: p.pid,
+            name: p.name,
+            cpu_percent: p.cpu.toFixed(2),
+            memory_percent: p.mem.toFixed(2),
+            command: p.command
+        })));
+    } catch (error) {
+        console.error('Error retrieving process list:', error.message);
+        res.status(500).json({ error: 'Failed to retrieve process list' });
     }
 });
--- src/routes/system.js
+++ src/routes/system.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const validate_api_key = require('../middleware/check_api_key');
-const { sys_status, cpu_info, memory_info, disk_info, os_info, ping} = require('../controllers/check_status');
+const { sys_status, cpu_info, memory_info, disk_info, os_info, network_info, process_info} = require('../controllers/check_status');
 const check_service = require('../middleware/check_service');
 
 router.post('/status', check_service('server_status'), validate_api_key, sys_status);
@@ -9,5 +9,7 @@ router.post('/cpu', check_service('cpu_info'), validate_api_key, cpu_info);
 router.post('/memory', check_service('memory_info'), validate_api_key, memory_info);
 router.post('/disk', check_service('disk_info'), validate_api_key, disk_info);
 router.post('/os', check_service('os_info'), validate_api_key, os_info);
+router.post('/network', check_service('network'), validate_api_key, network_info);
+router.post('/process', check_service('process'), validate_api_key, process_info);
 
 module.exports = router;
